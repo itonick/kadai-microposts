b38e8a31684677bcce60a2b99651f8690623a073
ito <yorizo.ito@gmail.com>
2020-06-17 12:14:51 +0000

    timeline

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -12,7 +12,7 @@ class UserFollowController extends Controller
         return back();
     }
     
-    public function unfollow($id)
+    public function destroy($id)
     {
         \Auth::user()->unfollow($id);
         return back();
